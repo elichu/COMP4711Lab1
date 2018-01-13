563e2dfff53b9b34d5c2abdb2b5e97169f103c68
elichu <elichu0@gmail.com>
2018-01-13 15:05:12 -0800

    Setup

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "hello it's me";
+        ?>
+    </body>
+</html>
+
+
